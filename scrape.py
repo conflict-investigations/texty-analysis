9b09fc38712fd4648d9ca947c5b298599a9f56cb
Anonymous <none@none.none>
2023-03-23 05:23:01 +0100

    Update data, add ipynb

--- scrape.py
+++ scrape.py
@@ -1,2 +1,4 @@
 # https://thallium.texty.org.ua/maps/shelling_map/ua_war_data_geojsons/2022-02-23.geojson
 BASE_API = 'https://thallium.texty.org.ua/maps/shelling_map/ua_war_data_geojsons'  # noqa
+
+CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSyFuA7nxANnn7BwXn7az5D5L-V7yKnETgTybfKSIGmoYz2qVkc6FWSH7f0l-1Gt_dML1VpywPUzXwp/pub?gid=1376631421&single=true&output=csv'
